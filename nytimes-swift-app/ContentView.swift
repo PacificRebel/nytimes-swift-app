6a7b520aa18b918768e5ef6c4a86e209fa71d6a4
Heli Sivunen <heli@mooncat.tv>
2021-08-21 16:51:25 +0100

    first commit

---
+++ nytimes-swift-app/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  nytimes-swift-app
+//
+//  Created by Heli Sivunen on 21/08/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ nytimes-swift-app/nytimes_swift_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  nytimes_swift_appApp.swift
+//  nytimes-swift-app
+//
+//  Created by Heli Sivunen on 21/08/2021.
+//
+
+import SwiftUI
+
+@main
+struct nytimes_swift_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
