7729df01af8e2efbe1af6e028614286c7894f202
Heli Sivunen <heli@mooncat.tv>
2021-08-21 17:38:12 +0100

    VStack, HStack, Spacer, Padding

--- nytimes-swift-app/ContentView.swift
+++ nytimes-swift-app/ContentView.swift
@@ -9,27 +9,25 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            VStack {
-                VStack(alignment: .center) {
+        VStack(alignment: .leading) {
                     Text("New York Times Top Stories")
                         .font(.title2)
                         .fontWeight(.bold)
                         .foregroundColor(Color.black)
-                        .multilineTextAlignment(.center)
-                        .padding()
-                }
+                
                 HStack {
                     Text("copyright NY Times")
                         .font(.subheadline)
                         .fontWeight(.light)
+                   
+                    Spacer()
                     Text("2021")
                         .font(.subheadline)
                         .fontWeight(.light)
                 }
             }
-            
-        }
+        .padding()
+        
     }
 }
 
@@ -37,4 +35,5 @@ struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
+
 }
